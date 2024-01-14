9b83736a898336f3039489f918414a5d6db6e11f
Jarvis1337 <77638593+Jarvis1337@users.noreply.github.com>
2024-01-14 21:22:47 +0000

    Enhancement in XversE-Adv-Handler v1.0 🚀

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const { Client, Intents } = require('discord.js');
-const config  = require('./config.json');
+const { Client, Intents } = require("discord.js");
+const config = require("./config.json");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.on('ready', () => {
-  console.log(`[>] ✅ | Logged in as ${client.user.tag}!`);
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.channel.send('Pong!');
+client.on("ready", () => {
+  client.user.setStatus("dnd");
+  client.user.setActivity("XversE-Adv-Handler", { type: "WATCHING" });
+  console.log(`[>] ✅ | ${client.user.username} is Ready to use!`);
+
+  client.application.commands.create({
+    name: "ping",
+    description: "Replies with Pong!",
+  });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { XintSlashCMD } = interaction;
+
+  if (XintSlashCMD === "ping") {
+    await interaction.reply("Pong!");
   }
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.content === "..ping") {
+    message.channel.send("Pong!");
+  }
+
+  if (message.content === "..reload") {
+    const commands = await client.application.commands.fetch();
+    for (const command of commands.values()) {
+      await command.delete();
+    }
+    message.channel.send('All slash commands have been reloaded...');
+  }
+
 });
 
 client.login(config.token || process.env.TOKEN);

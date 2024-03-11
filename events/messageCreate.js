15ba99e29032dd82f72e1b389dc3bba60af11da6
-`Ashborn <77638593+Jarvis1337@users.noreply.github.com>
2024-03-11 17:48:19 +0000

    🚀 Enhancement in XversE <3...
    🚀 Added Mention-About in XversE <3...

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,22 @@
 const config = require("../config.json");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = (client) => {
   client.on("messageCreate", async (message) => {
     let XprefiX = config.prefix;
+    var XversE = `<@${client.user.id}>`
+
+    if (message.content === XversE) {
+      message.reply({
+        embeds: [
+          new MessageEmbed()
+          .setTitle('<a:lnx_white_crown:969664230016040981> *Prefix*  <a:lnx_white_crown:969664230016040981>')
+          .setColor('000001')
+          .setDescription(`*My Prefix in this server is **\`${XprefiX}\`***`)
+        ],
+      });
+    }
+    
     if (!message.content.startsWith(XprefiX) || message.author.bot) return;
 
     const args = message.content.slice(XprefiX.length).trim().split(/ +/);
